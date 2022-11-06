bb65f33add0997e8724eb3e1eb8afe81f4c0b0d8
Hapsarip <hapsari.ndhari7@gmail.com>
2022-11-06 16:42:55 +0700

    adding nav bar and button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {
   Routes
 } from 'react-router-dom'
 import Login from "./pages/Login"
+import Nav from './components/nav'
 
 export default function App() {
   return (
     <Router>
+      <Nav/>
       <Routes>
         <Route path='/' element={<Login/>} />
       </Routes>

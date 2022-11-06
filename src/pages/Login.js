44713406c323a1bf66b59de232bc143a73773e2d
Hapsarip <hapsari.ndhari7@gmail.com>
2022-11-06 18:23:32 +0700

    edit login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,6 +17,14 @@ export default function Login() {
     }, [])
 
   return (
-    <div>Login</div>
+    <div className='flex w-full h-screen'>
+      <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
+        to be hide
+      </div>
+      <div className='w-full flex item-center items-center justify-center lg:w-1/2'>
+        form
+      </div>
+    </div>
+    
   )
 }

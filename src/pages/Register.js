332ccddd89a01cf1d843d947c544d33668d7fa16
Hapsarip <hapsari.ndhari7@gmail.com>
2022-11-13 16:50:55 +0700

    add background image

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,10 +4,10 @@ export default function Register() {
   return (
     <div className='flex w-full h-screen'>
       <div className='hidden lg:flex h-full w-1/2 items-center justify-center bg-gray-200'>
-      to be hide
+        <img class="max-w-none" src="./to-do-list.png" alt="bg img"/>
       </div>
       <div className='w-full flex item-center items-center justify-center lg:w-1/2'>
-      <RegisterForm/>
+        <RegisterForm/>
       </div>
     </div>  
   )

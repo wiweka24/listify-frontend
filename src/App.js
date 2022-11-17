3847b392b1b88b844625185a6bbe5427b8a6f69c
MuhammadZikriansyah <muh.zikriansyah@mail.ugm.ac.id>
2022-11-17 17:39:26 +0700

    UI Activity Form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom'
 import Login from "./pages/Login"
 import Nav from './components/nav'
+import Form from './pages/Form'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Nav/>
       <Routes>
         <Route path='/' element={<Login/>} />
+        <Route path='/form' element={<Form/>} />
       </Routes>
     </Router>
   )

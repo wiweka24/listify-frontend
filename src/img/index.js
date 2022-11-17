7ec34bc8c9a82fad12a4afa909a281e3a25dcbb4
Hapsarip <hapsari.ndhari7@gmail.com>
2022-11-17 19:13:41 +0700

    mau pull lagi

--- src/img/index.js
+++ src/img/index.js
@@ -3,11 +3,21 @@ import LoginImg from './to-do-list.png'
 import { ReactComponent as SearchIcon } from './search-icon.svg'
 import { ReactComponent as AddBtnIcon } from './add-btn-icon.svg'
 import { ReactComponent as ProfileIcon } from './profile-svgrepo-com.svg'
+import { ReactComponent as EditIcon } from './btn-pencil.svg'
+import { ReactComponent as ExecutionIcon } from './execution-date.svg'
+import { ReactComponent as CategoryIcon } from './label-icon.svg'
+import { ReactComponent as DeleteIcon } from './delete-icon.svg'
+
+
 
 export {
   Logo, 
   ProfileIcon, 
   LoginImg, 
   SearchIcon,
-  AddBtnIcon
+  AddBtnIcon,
+  EditIcon,
+  ExecutionIcon,
+  CategoryIcon,
+  DeleteIcon
 }

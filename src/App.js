54bd18b222cf5040ee7463583fafc0d9becada12
MuhammadZikriansyah <muh.zikriansyah@mail.ugm.ac.id>
2022-11-17 22:07:00 +0700

    Revisi UI Form

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import {
 import Login from "./pages/Login"
 import Activities from './pages/Activities'
 import Nav from './components/nav'
-<<<<<<< HEAD
 import Form from './pages/Form'
-=======
+
 import Register from './pages/Register'
 import Profile from './pages/Profile'
->>>>>>> d1b9be4892ecf2eeaee28452de33d9d15da6e2cc
 
 export default function App() {
   return (
@@ -19,13 +17,10 @@ export default function App() {
       <Nav/>
       <Routes>
         <Route path='/' element={<Login/>} />
-<<<<<<< HEAD
         <Route path='/form' element={<Form/>} />
-=======
         <Route path='/register' element={<Register/>} />
         <Route path='/activities' element={<Activities/>} />
         <Route path='/profile' element={<Profile/>} />
->>>>>>> d1b9be4892ecf2eeaee28452de33d9d15da6e2cc
       </Routes>
     </Router>
   )
--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -44,31 +44,42 @@ export default function ActivityForm(){
 //   };
 
   return(
-    <div className='flex w-full h-screen justify-center'>
+    <div className='flex w-full justify-center'>
       <div className='w-full md:w-5/6 lg:w-1/2 px-10 py-10'>
-        <h1 className='absolute font-extrabold text-black inline m-0 left-[325px] top-[100px] text-[40px] leading-[normal] flex text-5xl font-bold justify-center'>Add Activity</h1>
-        <div className = "md:flex mt-2">
-            <div className="absolute md:w-[25%] md:mr-[2%] text-3xl font-semibold text-black inline m-0 left-[459px] top-[259px] leading-[normal]">
-            Activity's Name
-            <input
-                className='font-normal py-1 px-1 w-full border-2 rounded-lg border-gray-500'
-                // id={values.ActivityName}
-                // onChange={handleChange("ActivityName")}
-                placeholder="Input Activity"
-            />
+        <h1 className='flex text-5xl font-extrabold justify-center'>Add Activity</h1>
+        <div className='flex justify-center h-[18vw] lg:h-[10vw]'>
         </div>
-        <div
-            className="absolute md:w-[25%] md:mr-[2%] text-3xl font-semibold text-black inline m-0 left-[459px] top-[401px] leading-[normal]">
-                Description
+        <div className='md:flex -mt-28 ml-18'>
+            <div className='md:w-[100%] md:mr-[2%]'>
+                <h3 className='text-3xl font-bold'>Activity's Name</h3>
+                <input 
+              className='py-1 px-2 w-full h-12 border-2 rounded-lg border-gray-500'
+              //value={activity.actActivity}
+              //readOnly
+              />
+              </div>
+            <div className="md:w-[100%] md:mr-[2%] ml-32 font-bold text-3xl">
+                Execution Date
             <input
-                className="font-normal py-1 px-1 w-full border-2 rounded-lg border-gray-500"
-                // id={values.Description}
-                // onChange={handleChange('Description')}
-                placeholder="Input Description"
+                className="font-normal py-1 px-1 h-12 w-full border-2 rounded-lg border-gray-500"
+                // id={values.date}
+                // onChange={handleChange('date')}
+                placeholder="Input Execution date"
+                type="date"
             />
-        </div>
+            </div>
+            </div>
+            <div className='md:flex mt-20 ml-18'>
+            <div className='md:w-[100%] md:mr-[2%] ml-18'>
+                <h3 className='text-3xl font-bold'>Description</h3>
+                <input 
+              className='py-1 px-2 w-full h-40 border-2 rounded-lg border-gray-500'
+              //value={activity.actActivity}
+              //readOnly
+              />
+              </div>
         <div
-            className="absolute md:w-[25%] md:mr-[2%] text-3xl font-semibold text-black inline m-0 left-[459px] top-[544px] leading-[normal]">
+            className="md:w-[100%] md:mr-[2%] ml-32 font-bold text-3xl">
                 Category
             <input
                 className="font-normal py-1 px-1 w-full border-2 rounded-lg border-gray-500"
@@ -78,18 +89,7 @@ export default function ActivityForm(){
             />
         </div>
         </div>
-        <div className = "md:flex mt-2">
-            <div className="absolute md:w-[25%] md:mr-[2%] text-3xl font-semibold text-black inline m-0 left-[1221px] top-[259px] leading-[normal]">
-                Execution Date
-            <input
-                className="font-normal py-1 px-1 w-full border-2 rounded-lg border-gray-500"
-                // id={values.date}
-                // onChange={handleChange('date')}
-                placeholder="Input Execution date"
-                type="date"
-            />
-        </div>
-        <div
+        {/* <div
             className="absolute md:w-[25%] md:mr-[2%] text-3xl font-semibold text-black inline m-0 left-[1222px] top-[404px] leading-[normal]">
                 Label
             <input
@@ -102,36 +102,36 @@ export default function ActivityForm(){
         <h3 class="flex absolute mb-4 text-3xl font-semibold text-gray-900 dark:text-white left-[1222px] top-[544px] leading-[normal]">Status</h3>
 <ul class="flex absolute items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
     <li class="flex absolute w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-        <div class="flex absolute items-center pl-3 left-[600px] top-[519px] leading-[normal]">
+        <div class="flex absolute items-center pl-3 left-[1172px] top-[524px] leading-[normal]">
             <input id="vue-checkbox-list" type="checkbox" value="" class="w-16 h-16 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
             <label for="vue-checkbox-list" class="py-3 ml-2 w-full text-2xl font-medium text-gray-900 dark:text-gray-300">Will be done</label>
         </div>
     </li>
     <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-        <div class="flex absolute items-center pl-3 left-[800px] top-[519px] leading-[normal]">
+        <div class="flex absolute items-center pl-3 left-[1382px] top-[524px] leading-[normal]">
             <input id="react-checkbox-list" type="checkbox" value="" class="w-16 h-16 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
             <label for="react-checkbox-list" class="py-3 ml-2 w-full text-2xl font-medium text-gray-900 dark:text-gray-300">Ongoing</label>
         </div>
     </li>
     <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-        <div class="flex absolute items-center pl-3 left-[960px] top-[519px] leading-[normal]">
+        <div class="flex absolute items-center pl-3 left-[1547px] top-[524px] leading-[normal]">
             <input id="angular-checkbox-list" type="checkbox" value="" class="w-16 h-16 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
             <label for="angular-checkbox-list" class="text-2xl py-3 ml-2 w-full font-medium text-gray-900 dark:text-gray-300">Done</label>
         </div>
     </li>
 </ul>
-</div>
-        <button
-            // onClick={this.continue}
-            className="w-3/5 md:w-2/5 bg-green-500 text-white py-2 px-6 rounded hover:bg-indigo-400 duration-500 absolute items-center inline m-0 left-[645px] top-[791px] text-[33px] leading-[normal] outline">
-            Add
-        </button>
+</div> */}
+<div className='mt-28 flex justify-center'>
+          <button className='text-2xl font-bold w-4/5 md:w-3/5 bg-green-500 text-white py-2 px-6 rounded-xl hover:bg-indigo-400 duration-500'>Add</button>
         </div>
-        <button 
-            className='w-3/5 md:w-2/5 bg-red-500 text-white py-2 px-6 rounded hover:bg-indigo-400 duration-500 absolute items-center left-[645px] top-[873px] text-[33px] leading-[normal] outline">'
+        <div className='mt-5 flex justify-center'>
+          <button 
+            className='text-2xl font-bold w-4/5 md:w-3/5 bg-red-500 text-white py-2 px-6 rounded-xl hover:bg-indigo-400 duration-500'
             //onClick={handleClick}
             >Cancel
           </button>
+        </div>
+    </div>
     </div>
     )
 }

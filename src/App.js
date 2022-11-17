8b0721d43310583058f1610927eacd4526b04745
kurniakdu <kurniakdu@gmail.com>
2022-11-17 16:42:38 +0700

    mau ngepull aja

--- src/App.js
+++ src/App.js
@@ -8,17 +8,16 @@ import Activities from './pages/Activity'
 import Nav from './components/nav'
 import Register from './pages/Register'
 import Profile from './pages/Profile'
-import Modal from './components/modal'
 
 export default function App() {
   return (
     <Router>
       <Nav/>
       <Routes>
-        <Route path='/' element={<Activities/>} />
-        {/* <Route path='/register' element={<Register/>} />
-        <Route path='/activity' element={<Activities/>} />
-        <Route path='/profile' element={<Profile/>} /> */}
+        <Route path='/' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/activity' element={<Activity/>} />
+        <Route path='/profile' element={<Profile/>} />
       </Routes>
     </Router>
   )

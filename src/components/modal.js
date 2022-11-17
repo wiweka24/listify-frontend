1c033240bf0a4e05e5d6f71a7a25ac8eb4ceb6e5
viraayuoktvn <viraayu01@mail.ugm.ac.id>
2022-11-17 15:10:56 +0700

    fixing errors

--- src/components/modal.js
+++ src/components/modal.js
@@ -10,7 +10,7 @@ export default function Modal({isVisible, onClose, children}) {
     return (
         <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'
         id='wrapper' onClick={handleClose}>
-            <div className='w-4/5 h-full flex flex-col'>
+            <div className='w-3/5 h-full flex flex-col'>
                 <button className="text-white text-xl place-self-end" onClick={() => onClose()}>X</button>
                 <div className='bg-white p-6 rounded-3xl'>
                     {children}
--- src/pages/Activity.js
+++ src/pages/Activity.js
@@ -8,35 +8,10 @@ import Modal from '../components/modal'
 export default function Activities() {
   return (
     <div className='flex w-full h-screen'>
-      <div className='w-full flex-col item-center items-top justify-center lg:w-full'>
-        
-        <div className="flex-auto justify-between mx-auto">
-          <div className='fixed flex justify-between bg-white w-full py-3 border-2 border-gray-100'>
-            <h1 className='justify-center py-4 px-5 item-center text-3xl font-bold'>Activity List</h1>
-                  
-            <div className="flex relative items-center">
-              <input className="text-white flex focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-full bg-blue-500 appearance-none w-3/4 text-sm leading-6 h-10 placeholder-white pl-10 rounded-full ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Search projects" placeholder="Search..."></input>
-              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <span className='text-xl text-white mr-1 pt-2'>
-                  <ion-icon name="search-outline" ></ion-icon>
-                </span>
-              </div>
-                <button className="flex items-center py-2 px-4 rounded items-center">
-                  <svg className="w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gray" strokeWidth={0.8} stroke="white">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
-                </button>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-      <div className='w-full flex-col justify-center lg:w-full'>
-        <Activity/>
-        <Activity/>
-        <Activity/>
+      <div className='w-full flex-col items-center justify-center lg:w-auto'>
+        <ActivityList/>
         <Activity/>
       </div>
-      
     </div>
   )
 }

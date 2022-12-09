d068743b5bfa0a50fd7c37786daf365c5cbb7517
wiweka24 <yogomu24@gmail.com>
2022-12-09 14:20:26 +0700

    filter act to match calender

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,55 +1,56 @@
-import { useState, useEffect, Fragment } from 'react'
+import { useState, Fragment } from 'react'
 import { ProfileIcon } from '../img'
 import { axiosInstance, URI, toastifyConfig } from "../components/component-config"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import Confirm from '../components/confirmation'
 
 export default function ProfileEdit() {
-  const navigate = useNavigate()
-  const [user, setUser] = useState([])
-  const [showConfirm, setShowConfirm] = useState(false)
-  const URL = URI + "/user/" + 'profile'
-  
-  useEffect(() => {  
-    (async () => {
-      try {
-        const res = await axiosInstance.get(URL)
-        setUser(res.data)
-      } catch(err) {
-        console.log(err)
-      }
-    })()
-  }, [])
+  const location = useLocation();
+  const { user } = location.state;
 
-  const handleChange = (e) => {
-    setUser({ ...user, [e.target.id]: e.target.value });
-  };
+  const [showConfirm, setShowConfirm] = useState(false)
+  const [userEdit, setUserEdit] = useState({
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    about: user.about
+  })
+
+  const navigate = useNavigate()
+  const URL = URI + "/user/profile"
+
   
   const handleClick = async (e) => {
     e.preventDefault()
-    try{
-      const res = await axiosInstance.patch(URL + '/'+ user._id,  
+    try {
+      const res = await axiosInstance.patch(URL,  
         {
-          usename: user.username,
-          email: user.email,
-          password: user.password,
+          username: userEdit.username,
+          email: userEdit.email,
+          password: userEdit.password,
+          about: userEdit.about
         })
-      console.log(res.data)
-      navigate("/profile")
-  
+        console.log(res.data)
+        navigate("/profile")
+        
     } catch (err) {
       console.error(err.response.data);
       toast.error(err.response.data.error, toastifyConfig)
     }
-  };
+  }
 
+  const handleChange = (e) => {
+    setUserEdit({ ...user, [e.target.id]: e.target.value });
+  };
+    
   const handleCancel = () => {
     navigate("/profile")
-  }
+  };
 
   return (
     <Fragment>
+    <ToastContainer/>
     <div className='flex w-full justify-center'>
       <div className='w-full md:w-5/6 lg:w-1/2 px-10 py-10'>
         <h1 className='flex text-4xl font-bold justify-center'>Profile</h1>
@@ -58,15 +59,21 @@ export default function ProfileEdit() {
         </div>
         <div className=''>
           <h3 className='text-xl font-semibold'>About</h3>
-          <p className='py-1 px-2 w-full h-20 border-2 rounded-lg border-gray-100 bg-blue-100'>test</p>
+          <textarea 
+              className='py-1 px-2 w-full h-20 border-2 rounded-lg border-gray-200'
+              id="about"
+              value={userEdit.about}
+              onChange={handleChange}
+              type='text'
+            />
         </div>
         <div className='md:flex mt-2'>
           <div className='md:w-[49%] md:mr-[2%]'>
             <h3 className='text-xl font-semibold'>Username</h3>
             <input 
-              className='py-1 px-2 w-full border-2 rounded-lg border-gray-100 bg-blue-100'
+              className='py-1 px-2 w-full border-2 rounded-lg border-gray-200'
               id="username"
-              value={user.username}
+              value={userEdit.username}
               onChange={handleChange}
               type='text'
             />
@@ -74,9 +81,9 @@ export default function ProfileEdit() {
           <div className='md:w-[49%] md:mt-0 mt-2'>
             <h3 className='text-xl font-semibold'>Email</h3>
             <input 
-              className='py-1 px-2 w-full border-2 rounded-lg border-gray-100 bg-blue-100'
+              className='py-1 px-2 w-full border-2 rounded-lg border-gray-200'
               id="email"
-              value={user.email}
+              value={userEdit.email}
               type='email'
               onChange={handleChange}
             />
@@ -85,10 +92,11 @@ export default function ProfileEdit() {
         <div className='md:w-[49%] mt-2'>
           <h3 className='text-xl font-semibold'>Password</h3>
           <input 
-            className='py-1 px-2 w-full border-2 rounded-lg border-gray-100 bg-blue-100'
-            value={user.password}
-            type='password'
-            // onChange={handleChange}
+            className='py-1 px-2 w-full border-2 rounded-lg border-gray-200'
+            id="password"
+            value={userEdit.password}
+            type="password"
+            onChange={handleChange}
           />
         </div>
         <div className='mt-16 flex justify-center'>
